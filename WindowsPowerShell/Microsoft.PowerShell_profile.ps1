7d04cd62b639e4e9b49fbbea3f70a592c5fbfbdc
NathanDV <76598451+NathanDV0@users.noreply.github.com>
2022-01-18 17:42:56 -0500

    Add files via upload

---
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+clear
+Invoke-Expression (&starship init powershell)
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
